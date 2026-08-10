41510511d91c87e2176a782717445868b733f7bb
2063907 Sharath <2063907@FOXLINK_IN1.com>
2026-08-10 14:28:44 +0530

    single save based on the requirement

--- A246FProject/obj/Debug/net8.0/A246FProject.AssemblyInfo.cs
+++ A246FProject/obj/Debug/net8.0/A246FProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A246FProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d7fdc6d1a816f6aed5df0eabdca8ac720dd9ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2eb7c10287cdafc12cfdf1c71da03c8fc30d928")]
 [assembly: System.Reflection.AssemblyProductAttribute("A246FProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A246FProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
